de93c136acdb6a182d1b2b95a39fe02586a09dc9
Ali Gholami Rudi <ali@rudi.ir>
2010-04-01 21:13:25 +0430

    add copyright notice

--- ld.c
+++ ld.c
@@ -1,3 +1,12 @@
+/*
+ * ld - a small static linker
+ *
+ * Copyright (C) 2010 Ali Gholami Rudi
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License, as published by the
+ * Free Software Foundation.
+ */
 #include <elf.h>
 #include <fcntl.h>
 #include <stdio.h>
